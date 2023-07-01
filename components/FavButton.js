53693a57663edd5b5bbc31275d798cc1553f7ca8
Manu Morante <manu@estadologico.com>
2023-07-01 08:21:28 +0200

    eslint

--- components/FavButton.js
+++ components/FavButton.js
@@ -56,7 +56,6 @@ export default function FavButton({ initialState = false, onClick }) {
   // Shared values
   const activeColor = '#eab308' // text-yellow-500
   const inactiveColor = '#6b7280' // text-gray-500
-  const duration = 0.5
   const ease = 'easeInOut'
 
   return (
@@ -74,7 +73,7 @@ export default function FavButton({ initialState = false, onClick }) {
           animate={{
             scale: active ? [null, 2, 1] : hover ? [null, 1.2, 1.1] : [null, 1.2, 1],
             transition: {
-              duration,
+              duration: 0.5,
               ease,
               times: [0, 0.4, 1],
             },
@@ -86,15 +85,18 @@ export default function FavButton({ initialState = false, onClick }) {
             //
             stroke={active ? activeColor : inactiveColor}
             fill="none"
-            initial={{
-              pathLength: active ? 0 : 1,
-              opacity: active ? 0 : 1,
-            }}
-            animate={{
-              pathLength: active ? 0 : 1,
-              opacity: active ? 0 : 1,
-              stroke: hover ? activeColor : inactiveColor,
-            }}
+            initial={
+              active
+                ? { pathLength: 0, opacity: 0 } //
+                : { pathLength: 1, opacity: 1 }
+            }
+            animate={
+              active
+                ? { pathLength: 0, opacity: 0 } //
+                : hover
+                ? { stroke: activeColor }
+                : { stroke: inactiveColor }
+            }
             transition={{
               duration: 0.3,
               opacity: { duration: 0.4 },
@@ -106,18 +108,17 @@ export default function FavButton({ initialState = false, onClick }) {
             // Fill
             //
             fill={active ? activeColor : inactiveColor}
-            initial={{
-              opacity: active ? 1 : 0,
-              scale: active ? 1 : 0,
-            }}
-            animate={{
-              opacity: active ? 1 : 0,
-              scale: active ? 1 : 0,
-            }}
-            transition={{
-              duration,
-              opacity: { duration: 0.1 },
-            }}
+            initial={
+              active
+                ? { opacity: 1, scale: 1 } //
+                : { opacity: 0, scale: 0 }
+            }
+            animate={
+              active
+                ? { opacity: 1, scale: 1 } //
+                : { opacity: 0, scale: 0 }
+            }
+            transition={{ duration: 0.5, opacity: { duration: 0.1 } }}
             d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
           />
         </motion.svg>
@@ -131,7 +132,7 @@ export default function FavButton({ initialState = false, onClick }) {
             scale: active ? [0, 2, 2.2] : 0,
             opacity: active ? [1, 0.8, 0] : 0,
             transition: {
-              duration,
+              duration: 0.5,
               times: [0, 0.4, 1],
             },
           }}
